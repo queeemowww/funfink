3839a7b57cb6b4a50405a017274c63719dc9e67b
Gleb <glebkuimov@gmail.com>
2025-11-04 18:20:20 +0300

    suka

--- funding_rate_positions.py
+++ funding_rate_positions.py
@@ -116,8 +116,8 @@ class Logic():
         self.diff_return=0.15
         #время
         self.check_price_start=5
-        self.check_price_finish=44
-        self.minutes_for_start_parse=45
+        self.check_price_finish=20
+        self.minutes_for_start_parse=21
         # ===== Настройки =====
         self.take_risk_size=0.2
         self.TIMEOUT = httpx.Timeout(15.0, connect=15.0, read=15.0)
