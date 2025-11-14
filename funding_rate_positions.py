7fc954321287db886da5e5911bd7e40d2b448fda
Gleb <glebkuimov@gmail.com>
2025-11-14 21:16:10 +0300

    sdf

--- funding_rate_positions.py
+++ funding_rate_positions.py
@@ -1801,15 +1801,15 @@ class Logic():
                 await asyncio.sleep(60)
 
     async def main(self):
-        symbol = 'BIOUSDT'
+        symbol = 'SWARMSUSDT'
         print("OPEN LONG:", await self.c.dict['htx'].open_long(symbol = symbol, qty = 300, leverage=5, order_type="Market"))
         print("POSITIONS:", await self.c.dict['htx'].get_open_positions(symbol))
         print("POSITIONS:", await self.c.dict['htx']._all_positions())
-        print("LAST PRICE: ", self.get_last_price_htx("BIO/USDT"))
+        print("LAST PRICE: ", self.get_last_price_htx(self.convert_symbol_for_exchanges('htx',"SWARMS/USDT")))
 
         # Закрываем обе стороны безопасно (не упадёт по RuntimeError)
         print("CLOSE ALL:", await self.c.dict['htx'].close_all_positions(symbol=symbol))
-        # await asyncio.gather(self.run_window(), self.run_at_50(), self.run_daily_task())
+        await asyncio.gather(self.run_window(), self.run_at_50(), self.run_daily_task())
         
 
 if __name__ == "__main__":
