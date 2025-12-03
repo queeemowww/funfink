76b1a296b263d02e695e0d944786a01a548abbff
Gleb <glebkuimov@gmail.com>
2025-12-03 19:36:29 +0300

    suka

--- funding_rate_positions.py
+++ funding_rate_positions.py
@@ -1968,20 +1968,6 @@ class Logic():
                         
                         
                         current_old_diff = ((long_price - active_logs.iloc[i]['long_price']) / active_logs.iloc[i]['long_price'] - (short_price - active_logs.iloc[i]['short_price']) /  active_logs.iloc[i]['short_price']) *100
-                        if seconds_15 < 20:
-                            self.diff_return = 0.5 - 0.8 * possible_revenue
-                        elif 20 <= seconds_15 < 30:
-                            self.diff_return = 0.45 - possible_revenue
-                        elif 30 <= seconds_15 < self.minutes_for_start_parse:
-                            self.diff_return = 0.4 - possible_revenue
-                        else:
-                            self.diff_return = 0.5
-
-                        print("current long ptice", long_price, "open long price", active_logs.iloc[i]['long_price'])
-                        print("current short ptice", short_price,"open short price", active_logs.iloc[i]['short_price'])
-                        print(current_old_diff, self.diff_return)
-                        self.tg_send(spam=True, text=f"current long price = {long_price:.4f}, open long price = {active_logs.iloc[i]['long_price']:.4f}\ncurrent short price = {short_price:.4f}, open short price = {active_logs.iloc[i]['short_price']:.4f}\ncurrent diff = {current_old_diff:.4f}, demanded diff return = {self.diff_return:.4f}")
-
                         try:
                             self.confirmations[symbol] = self.confirmations[symbol]
                         except:
